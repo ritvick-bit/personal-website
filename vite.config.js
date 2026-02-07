d09a0ed9d9f0833a65c3330aa717db92d53eff52
Ritvick Palanikumar <Ritvick.official@gmail.com>
2026-02-07 15:22:44 -0600

    Update content and structure: WhatIDo, GreenBackground, Contact

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Website/', // Set base path for GitHub Pages matching Repo Name
+  base: '/personal-website/', // Set base path for GitHub Pages matching Repo Name
 })
